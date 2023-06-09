e5059309a52ec1c10e5f36fde4cff80bd2dc340c
Akira Nomura <anomura824@gmail.com>
2023-06-09 15:50:13 +0200

    first step

--- src/pyrepo/algos.py
+++ src/pyrepo/algos.py
@@ -1,5 +1,13 @@
 import numpy as np
+import pandas as pd
 
 def add_one(x):
     return x + 1
 
+def add_three(x):
+    return x + 3
+
+def add_one_to_df(x):
+    x = add_one(x)
+    out = pd.DataFrame(x)
+    return out
